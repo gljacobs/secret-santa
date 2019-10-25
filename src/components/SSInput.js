071460ce10ed8f450634e0d71ddb41621da251f7
Gabriel Jacobs <gabriel.luke.jacobs@gmail.com>
2019-10-25 00:03:16 -0700

    set up modal and add test data

--- src/components/SSInput.js
+++ src/components/SSInput.js
@@ -14,7 +14,7 @@ class SSInput extends React.Component {
     render() {
         return (
             <div className="input-group mb-3">
-                <input id="ssIn" type="text" className="form-control" placeholder="Enter Participant for Secret Santa..." value={this.state.value} onChange={this.handleChange}/>
+                <input id="ssIn" type="text" className="form-control" placeholder="Enter a participant for Secret Santa..." value={this.state.value} onChange={this.handleChange}/>
                 <div className="input-group-append">
                     <button className="btn btn-success" type="button" id="button-addon2" onClick={() => this.handleClick(this.state.value)}>+</button>
                 </div>
--- src/components/SSList.js
+++ src/components/SSList.js
@@ -3,14 +3,18 @@ import SSInput from './SSInput'
 
 class SSList extends React.Component {
     state = {
-        santas: [],
+        santas: [
+            { num: 1, name: "Gabriel", reveal: true, secret: "Mariah",},
+            { num: 2, name: "Mariah", reveal: true, secret: "Gabriel",}
+        ],
+        revealTarget: null,
     }
-    
+
     handleAddSanta = (input) => {
         this.setState({
             santas: [
                 ...this.state.santas,
-                { num: this.state.santas.length + 1, name: input, reveal: false }
+                { num: this.state.santas.length + 1, name: input, reveal: false, secret: null}
             ],
             value: ""
         })
@@ -19,7 +23,7 @@ class SSList extends React.Component {
         return (
             <div className="container">
                 <br />
-                <SSInput handleAddSanta={this.handleAddSanta}/>
+                <SSInput handleAddSanta={this.handleAddSanta} />
                 <table className="table table-striped table-bordered table-dark">
                     <thead className="bg-success">
                         <tr>
@@ -36,14 +40,31 @@ class SSList extends React.Component {
                                     <tr key={row.num}>
                                         <th scope="row">{row.num}</th>
                                         <td>{row.name}</td>
-                                        <td><button type="button" className="btn btn-success float-right btn-sm">Reveal</button></td>
+                                        <td><button type="button" className="btn btn-success float-right btn-sm" onClick={() => this.setState({revealTarget: row.num})} data-toggle="modal" data-target="#exampleModalCenter">Reveal</button></td>
                                     </tr>
                                 )
                             })
-
                         }
                     </tbody>
                 </table>
+                <div className="modal fade" id="exampleModalCenter" tabIndex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                    <div className="modal-dialog modal-dialog-centered" role="document">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title" id="exampleModalCenterTitle">Merry Christmas!</h5>
+                                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div className="modal-body">
+                                {this.state.revealTarget ? (this.state.santas[this.state.revealTarget - 1].name + " has " + this.state.santas[this.state.revealTarget - 1].secret + "...") : "..."}
+                            </div>
+                            <div className="modal-footer">
+                                <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
