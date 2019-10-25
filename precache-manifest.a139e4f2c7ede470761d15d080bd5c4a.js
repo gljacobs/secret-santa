1f0126a7e9c57f25c09c8bbb3338fc06b59f7de0
Gabriel Jacobs <gabriel.luke.jacobs@gmail.com>
2019-10-24 23:17:17 -0700

    Updates

---
+++ precache-manifest.a139e4f2c7ede470761d15d080bd5c4a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffff3dfae6f7860dcf03f7e2a5738574",
+    "url": "/secret-santa/index.html"
+  },
+  {
+    "revision": "3a7cd0962f91c1d5700d",
+    "url": "/secret-santa/static/css/main.2e64b6ea.chunk.css"
+  },
+  {
+    "revision": "f2fec06a08b83442d2f6",
+    "url": "/secret-santa/static/js/2.d8e43da0.chunk.js"
+  },
+  {
+    "revision": "3a7cd0962f91c1d5700d",
+    "url": "/secret-santa/static/js/main.67b3b1cf.chunk.js"
+  },
+  {
+    "revision": "499e8fc65f958162fbc7",
+    "url": "/secret-santa/static/js/runtime-main.5ddfd8b9.js"
+  }
+]);
